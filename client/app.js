4aa053cdcd6225f8a47875970435fe1e700797d4
Hoang Doan <hoang.doan@outlook.com>
2016-07-08 00:02:39 +0700

    Angular

---
+++ client/app.js
@@ -0,0 +1,42 @@
+var myAngular = angular.module("myAngular", ["ui.router"]);
+
+myAngular.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/home");
+
+  $stateProvider.state('home', {
+    url: "/home",
+    templateUrl: "templates/home.html",
+    controller: "HomeController",
+    params: {girl:null,index:null}
+  });
+
+  $stateProvider.state('add-girl', {
+    url: "/add-girl",
+    templateUrl: "templates/add-girl.html",
+    controller: "LXController"
+  });
+
+  $stateProvider.state('about-us', {
+    url: "/about-us",
+    templateUrl: "templates/about-us.html"
+  });
+  $stateProvider.state('update', {
+    url: "/update",
+    templateUrl: "templates/update.html",
+    controller: "update",
+    params : {girl: null,index: null}
+  });
+
+});
+
+myAngular.directive("lx",[function(){
+  return {
+    restrict: "A",
+    templateUrl: "templates/home.html",
+    scope:{
+      page: "=",
+      size: "="
+    },
+    controller :"HomeController"
+  }
+}])
